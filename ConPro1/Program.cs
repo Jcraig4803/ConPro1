f6142b02543801fdacfa6ccfa7439413328858a1
Johnathan Craig (jcraig4803) <johnathancraig1719@yahoo.com>
2016-07-05 21:34:18 -0400

    comput feed back
    inputing and giving feed back of user input

---
+++ ConPro1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConPro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string admin_name = "johnathan";
+            string admin_email = "johnathancraig1719@yahoo.com";
+            
+            string user_input = "";
+            string user_name = "";
+            string user_age = "";
+            string user_phonenumber = "";
+
+            Console.WriteLine("Tell me your name.");
+            user_name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + user_name + " Tell me your age, ");
+            user_age = Console.ReadLine();
+
+            Console.WriteLine("Tell me your phone number"+ user_name);
+            user_phonenumber = Console.ReadLine();
+
+            Console.WriteLine("So your name is "+ user_name + " you are " + user_age + " years old" + " and your phone number is " + user_phonenumber);
+
+
+            Console.ReadLine();
+        }
+    }
+}
